56269568af923c1807311d9a4288e79f1147e420
Yeferson Camilo Zaque <zaquecamilo@gmail.com>
2022-02-21 12:26:01 -0500

    Finalizada la primera versión del login, se emuló la respuesta del servidor (Malas practicas, revisar)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
@@ -13,6 +14,7 @@ import { IonicModule } from '@ionic/angular';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule,
     IonicModule.forRoot()
   ],
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,42 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
+interface Pizza {
+  name: string;
+  time: string;
+  image: string;
+}
+
+interface Complemento {
+  name: string;
+  image: string;
+}
+
+interface ResponseServer {
+  bebidas: Complemento[];
+  empanadas: Complemento[];
+  pizzas: Pizza[];
+  postres: Complemento[];
+}
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
+
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+  responseHttp: any; 
 
   ngOnInit(): void {
+    this.responseHttp = {
+      pizzas: [],
+      empanadas: [],
+      bebidas: [],
+      postres: []
+    }
+    this.http.get('../../assets/response.json').subscribe(data => { this.responseHttp = data });
   }
-
+  
 }
